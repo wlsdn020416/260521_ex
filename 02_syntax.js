059b13d78dc7ba1372a8b25cb840baf4cbe589f3
wlsdn020416 <wlsdn020416@gmail.com>
2026-05-21 11:13:50 +0900

    script 3

---
+++ 02_syntax.js
@@ -0,0 +1,24 @@
+//네임스페이스 명명
+// js -> 이미 사용중인 키워드와 $_를 제외한 모든 특수문자 와 공백 사용불가 
+//숫자로 시작할 수 없음
+
+// 약속 case
+
+// camelCase
+// yourName
+
+// snake_case
+// your_name
+
+// PascalCase
+// YourName
+
+// kebab-case
+// your-name
+
+//키워드 - 미리 빼놓은 단어
+// var, let, const, if, else, for, while, function, return, class, import, export 등등
+
+// ;는 자바스크립트에서 문장의 끝을 나타내는 구분자
+//javascript ';' -> 별 문제 없다 싶으면 javascript 가 알아서 붙여줌
+//뒤에 무엇이 올거라고 예상되는 키워드나 () [] {} 등은 알아서 붙여주지 않음
