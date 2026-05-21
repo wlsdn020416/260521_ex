7d0a5a3aaf4d06674f97554d6cd0c8b61625c6b9
wlsdn020416 <wlsdn020416@gmail.com>
2026-05-21 12:22:00 +0900

    script 8

---
+++ 05_objects-arrays.js
@@ -0,0 +1,30 @@
+//배열과 객체
+//for -> '배열','객체'
+const a1 = "apple";
+const a2 = "banana";
+const a3 = "orange";
+console.log(a1, a2, a3);
+
+const fruits = ["apple", "banana", "orange"];
+console.log(fruits);
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+fruits[2] = "grape";
+console.log(fruits);
+
+console.log(fruits.length);
+
+//객체 object
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
