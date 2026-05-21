693edfb3d27883adfa571097527efed75f9a85f5
wlsdn020416 <wlsdn020416@gmail.com>
2026-05-21 11:24:26 +0900

    script 4

---
+++ 03_var.js
@@ -0,0 +1,9 @@
+//변수 : 특정한 메모리의 주소에 값을 저장하는 공간
+name = "홍길동";
+console.log(name);
+// let 키워드
+let foo; // let 변수명은 변수명으로 메모리에 저장하겠다 라는 선언
+foo = 1;
+console.log(foo);
+let bar = 10;
+console.log(bar);
