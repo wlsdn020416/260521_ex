0c61287cf9a087759a5594db34c9247c0ef17f3b
wlsdn020416 <wlsdn020416@gmail.com>
2026-05-21 10:09:23 +0900

    script 1

---
+++ 01_script.js
@@ -0,0 +1,9 @@
+console.log("hello js!");
+        alert("hello js!");
+        const t = prompt ("what is your name?");
+        console.log(t);
+        if(confirm("are you sure?")){
+            console.log("yes");
+        } else {
+            console.log("no");
+        }
